5ba38a4170d3f4eaa32b6a9d3d196fcf385e5b8d
Sandy Macdonald <sandyjmacdonald@gmail.com>
2019-06-10 12:06:31 +0100

    Fixing missing bit in hours register, general tidyup

--- examples/set-time.py
+++ examples/set-time.py
@@ -4,23 +4,28 @@ import rv3028
 import time
 import datetime
 
-print("""set-time.py - Sets RTC with current time for your system and enble battery back-up to preserve the time when power is off
+print("""set-time.py - Sets RTC with current time for your system and
+enables battery backup to preserve the time when power is off.
 
 Press Ctrl+C to exit.
 
 """)
 
+# Create RV3028 instance
 rtc = rv3028.RV3028()
-# Switches rtc to backup battery of vcc goes below 2V other settings 'switchover_disabled', 'direct_switching_mode', 'standy_mode'
+
+# Switches RTC to backup battery if VCC goes below 2V
+# Other settings: 'switchover_disabled', 'direct_switching_mode', 'standby_mode'
 rtc.set_battery_switchover('level_switching_mode')
 
 try:
     current_system_time = datetime.datetime.now()
-    rtc.set_time_and_date(current_system_time)  # Time and date may also be set as a tuple (hour, minute, second, year, month, date)
-    while True:
-        rtc_time = rtc.get_time_and_date()
+    # Time and date may also be set as a tuple (hour, minute, second, year, month, date)
+    rtc.set_time_and_date(current_system_time)
 
-        print(("The date is {0}/{1}/{2} at {3}:{4}:{5}").format(rtc_time.day, rtc_time.month, rtc_time.year, rtc_time.hour, rtc_time.minute, rtc_time.second))
+    while True:
+        rtc_time = rtc.get_time()
+        print("The time is: {:02d}:{:02d}:{:02d} on :{:02d}/{:02d}/{:02d}".format(rtc_time.hour, rtc_time.minute, rtc_time.second, rtc_time.day, rtc_time.month, rtc_time.year))
         time.sleep(1)
 
 except KeyboardInterrupt:
